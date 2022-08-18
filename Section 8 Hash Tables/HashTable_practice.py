0cd1ab5085814e6ebcefd93b89016f29c273ce89
Rahul Raoniar <rahul.raoniar@outlook.com>
2022-08-18 14:27:22 +0530

    Added interview question

--- Section 8 Hash Tables/HashTable_practice.py
+++ Section 8 Hash Tables/HashTable_practice.py
@@ -1,10 +1,8 @@
-import enum
-
-
 class HashTable:
     def __init__(self, size = 7):
         self.data_map = [None] * size
         
+    # O(1)    
     def __hash(self, key):
         my_hash = 0
         for letter in key:
@@ -13,21 +11,31 @@ class HashTable:
     
     def print_table(self):
         for i, val in enumerate(self.data_map):
-            print(i, ": ", val)
+            print(i, " :", val)
             
     def set_item(self, key, value):
         index = self.__hash(key)
         if self.data_map[index] is None:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
-        
+    
+    # Normal case O(1), as it fairly distributes the items
     def get_item(self, key):
         index = self.__hash(key)
         if self.data_map[index] is not None:
-            for i in range(len(self.data_map[index])):
+            for i in range(len(self.data_map)):
                 if self.data_map[index][i][0] == key:
                     return self.data_map[index][i][1]
         return None
+    
+    def get_keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+                
         
             
 my_hash_table = HashTable()
@@ -35,4 +43,4 @@ my_hash_table.set_item("boot", 123)
 my_hash_table.set_item("apple", 111)
 my_hash_table.set_item("hats", 89)
 
-print(my_hash_table.get_item("apple"))
+print(my_hash_table.get_keys())
---
+++ Section 8 Hash Tables/Interview_Question.py
@@ -0,0 +1,31 @@
+# Find the common item
+
+# Method 1: O(n^2)
+# def item_in_common(list1, list2):
+#     for i in list1:
+#         for j in list2:
+#             if i==j:
+#                 return True
+#     return False
+                
+            
+# list1 = [1,3,5]
+# list2 = [2,4,5]
+# print(item_in_common(list1, list2))
+
+# Method 2: O(n)
+def item_in_common(list1, list2):
+    my_dict = {}
+    
+    for i in list1:
+        my_dict[i] = True
+    
+    for j in list2:
+        if j in my_dict:
+            return True
+        
+
+
+list1 = [1,3,5]
+list2 = [2,4,5]
+print(item_in_common(list1, list2))
